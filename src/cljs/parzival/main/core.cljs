a1fc6a7785215bc76864db8dbf4385a38004f25f
antonmoller <anton.moller.4666@student.uu.se>
2021-03-29 00:09:10 +0200

    Added the main folder

---
+++ src/cljs/parzival/main/core.cljs
@@ -0,0 +1,28 @@
+(ns parzival.main.core
+  (:require
+            ["electron" :refer [app BrowserWindow ipcMain shell]]))
+
+(def main-window (atom nil))
+
+(defn init-browser
+  []
+  (reset! main-window (BrowserWindow.
+                        (clj->js {:width 800
+                                  :height 600
+                                  :autoHideMenuBar true
+                                  :webPreferences {:nodeIntegration true
+                                                   :worldSafeExecuteJavaScript true
+                                                   :nodeIntegrationWorker true}})))
+  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil))
+  (.. ^js @main-window -webContents (on "new-window" (fn [e url]
+                                                       (.. e preventDefault)
+                                                       (.. shell (openExternal url))))))
+
+
+(defn main
+  []
+
+  (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
+                                  (.quit app)))
+  (.on  app "ready" init-browser))
