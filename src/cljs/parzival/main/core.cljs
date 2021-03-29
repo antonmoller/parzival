aca6e7d2006fbe048c24dd522c2ac88342c29951
antonmoller <anton.moller.4666@student.uu.se>
2021-03-29 14:46:35 +0200

    Electron now resizes, but something is fucked up. Two possible reasons: 1. xserver 2. re-frame-10x

--- src/cljs/parzival/main/core.cljs
+++ src/cljs/parzival/main/core.cljs
@@ -9,12 +9,15 @@
   (reset! main-window (BrowserWindow.
                         (clj->js {:width 800
                                   :height 600
+                                  :resize true
                                   :autoHideMenuBar true
+                                  :enableRemoteModule true
                                   :webPreferences {:nodeIntegration true
                                                    :worldSafeExecuteJavaScript true
+                                                   :enableRemoteModule true
                                                    :nodeIntegrationWorker true}})))
-  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil))
+  (.loadURL ^js @main-window (str "file://" js/__dirname "/public/index.html"))
+  (.on ^js @main-window "closed" #(reset! main-window nil))
   (.. ^js @main-window -webContents (on "new-window" (fn [e url]
                                                        (.. e preventDefault)
                                                        (.. shell (openExternal url))))))
@@ -22,7 +25,10 @@
 
 (defn main
   []
-
   (.on app "window-all-closed" #(when-not (= js/process.platform "darwin")
                                   (.quit app)))
-  (.on  app "ready" init-browser))
+  (.on app "activate" (fn []
+                        (when (nil? @main-window)
+                          (init-browser))))
+  (.on  app "ready" (fn []
+                      (init-browser))))
