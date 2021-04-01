ce25e52efd3985a770b45e4e47f5d7f201cdce6b
antonmoller <anton.moller.4666@student.uu.se>
2021-04-01 15:00:11 +0200

    Removing the backdrop from the modal was stupid

--- src/cljs/parzival/views/modal.cljs
+++ src/cljs/parzival/views/modal.cljs
@@ -4,19 +4,35 @@
             [parzival.style :refer [color ZINDICES DEPTH-SHADOWS]]
             [stylefy.core :as stylefy :refer [use-style]]))
 
+;;; Styles
+
+(def modal-container-style
+ {:z-index (:zindex-modal ZINDICES)
+  :position "fixed"
+  :inset "0px"
+  :width "100vw"
+  :height "100vh"
+  :display "flex"
+  :justify-content "center"
+  :align-items "center"})
+
+(def backdrop-style
+  {:position "absolute"
+   :inset "0px"
+   :background-color (color :background-color :opacity-low)})
+
 (def modal-style
-  {:z-index (:zindex-modal ZINDICES)
-   :position "fixed"
-   :top "50vh"
-   :left "50vw"
-   :transform "translate(-50%, -50%)"
-   :background-clip "padding-box"
-   :border-radius "0.25rem"
-   :background (color :background-1-color)
-   :box-shadow    [[(:64 DEPTH-SHADOWS) ", 0 0 0 1px " (color :background-color :opacity-lower)]]
-   :animation "fade-in 0.2s"})
+{:position "relative"
+ :z-index 1 
+ :border-radius "0.25rem"
+ :background-color (color :background-plus-1-color)
+ :box-shadow    [[(:64 DEPTH-SHADOWS) ", 0 0 0 1px " (color :body-text-color :opacity-lower)]]})
+
+;;; Components
 
 (defn modal
   [toggle content]
-  [:div (use-style modal-style {:on-click #(dispatch [toggle])})
-    content])
+  [:div (use-style modal-container-style)
+   [:div (use-style backdrop-style {:on-click #(dispatch [toggle])})]
+   [:div (use-style modal-style)
+    content]])
